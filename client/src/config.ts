cc47dd050d14b83a938b9c0b5d6776136512b3ea
lawalgodwin <godwinl200@gmail.com>
2022-11-20 13:28:04 +0100

    Fix Authentencation and Authorization bugs

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '59gdjuojb5'
+const apiId = 'x43lmkbak6'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map. For example:
-  // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: 'dev-6g99gwrj.us.auth0.com',            // Auth0 domain
-  clientId: 'JC3z8L8Y1GFfqKBiU4CZyui6AnAiR3WO',          // Auth0 client id
+  domain: 'dev-6g99gwrj.us.auth0.com',
+  clientId: 'JC3z8L8Y1GFfqKBiU4CZyui6AnAiR3WO',
   callbackUrl: 'http://localhost:3000/callback'
 }
